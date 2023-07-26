18eaa86590747c613bdedf2bdcb3c5823be689c2
Ethan4t0r <62487806+ethanfs20@users.noreply.github.com>
2023-07-26 01:00:01 -0400

    Update config.lua

--- driftmode/config.lua
+++ driftmode/config.lua
@@ -4,13 +4,21 @@ Config = {
     -- https://forum.cfx.re/t/basic-aces-principals-overview-guide/90917
 
     vehicleWhitelist = false,
-    models = { "adder" },
+    models = {"adder"},
     -- https://docs.fivem.net/docs/game-references/vehicle-models/
 
     vehicleClassWhitelist = false,
-    classes = { 0, 1, 2, 3, 4, 5, 6, 7},
+    classes = {0, 1, 2, 3, 4, 5, 6, 7},
     -- https://docs.fivem.net/natives/?_0x29439776AAA00A62
-    
+
     keybind = false,
-    startKey = 38
+    startKey = 38,
+
+    twostep = false,
+    handleMods = {
+        {"fInitialDragCoeff", 90.22}, {"fDriveInertia", .31},
+        {"fSteeringLock", 22}, {"fTractionCurveMax", -1.1},
+        {"fTractionCurveMin", -.4}, {"fTractionCurveLateral", 2.5},
+        {"fLowSpeedTractionLossMult", -.57}
+    }
 }
